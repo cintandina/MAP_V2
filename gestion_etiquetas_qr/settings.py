6a8d245d3c574e295af3ef6e269b930bf69577aa
cjsanchez-cintandina <cj.sanchez@cintandina.com>
2026-03-12 10:23:24 -0500

    Fix configuracion Cloudinary

--- gestion_etiquetas_qr/settings.py
+++ gestion_etiquetas_qr/settings.py
@@ -103,25 +103,20 @@ if ENVIRONMENT == 'production':
 
 USE_S3 = os.getenv('USE_S3', 'True') == 'True'
 
-# Configuración de Amazon S3 para archivos multimedia
 if USE_S3:
     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-
     AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
     AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
     AWS_S3_REGION_NAME = os.getenv('AWS_S3_REGION_NAME', 'us-east-2')
-
-    # dominio regional (recomendado)
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com'
-
     AWS_QUERYSTRING_AUTH = False
     AWS_S3_FILE_OVERWRITE = False
     AWS_DEFAULT_ACL = None
     AWS_S3_SIGNATURE_VERSION = "s3v4"
-
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/'
 else:
+    DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
     MEDIA_URL = '/media/'
     MEDIA_ROOT = BASE_DIR / 'media'
 
